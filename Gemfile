fb46e0fd8832ad0a15cbe721514df22151e11e2c
Zaur22 <mr.zaur22@gmail.com>
2016-01-30 12:32:43 +0300

    new repository

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
 gem 'slim'
+gem "paperclip", "~> 4.3"
 
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
   	events  = Event.where("DATETIME(date_and_time > ?)", DateTime.current)
   	places  = Place.order("created_at").all
   	@objects = events + places
+  	@event_categories = EventCategory.all
   end
 end
